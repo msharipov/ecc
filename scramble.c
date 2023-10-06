583b856897d7152bf08044d597ba607d6cbcacc4
msharipov <msharipovr@gmail.com>
2023-10-05 23:49:56 -0400

    added file opening

--- scramble.c
+++ scramble.c
@@ -1,5 +1,25 @@
 // Scrambles the file by flipping each bit with frequency p.
+#include <stdio.h>
 
 int main(int argc, char * argv[]) {
+
+    double P = 0.1;
+    srand(119);
     
+    FILE * infile = fopen(argv[1], "r"),
+         * outfile = fopen("out.txt", "w");
+
+    if (!infile) {
+        printf("Error! Cannot open %s", argv[1]);
+        return 1;
+    }
+
+    if (!outfile) {
+        printf("Error! Cannot open out.txt");
+        return 1;
+    }
+
+
+    fclose(infile);
+    return 0;
 }
