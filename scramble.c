507fcc845fc5ddbb23e5f1ef8b76a4d2634c5c87
msharipov <msharipovr@gmail.com>
2023-10-06 00:14:35 -0400

    added color to error messages

--- scramble.c
+++ scramble.c
@@ -9,12 +9,12 @@ int main(int argc, char * argv[]) {
          * outfile = fopen("out.txt", "w");
 
     if (!infile) {
-        printf("Error! Cannot open %s", argv[1]);
+        printf("\x1b[1;31mError!\x1b[0m Cannot open %s\n", argv[1]);
         return 1;
     }
 
     if (!outfile) {
-        printf("Error! Cannot open out.txt");
+        printf("\x1b[1;31mError!\x1b[0m Cannot open out.txt\n");
         return 1;
     }
 
