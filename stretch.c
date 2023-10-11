7ffab99f8aede3f8a9bdbcb72abe89809f20169f
msharipov <msharipovr@gmail.com>
2023-10-10 23:24:32 -0400

    fixed LEN checking in arguments

--- stretch.c
+++ stretch.c
@@ -3,7 +3,25 @@
 
 int main(int argc, char * argv[]) {
 
-    long length = 1;
+    long LEN = 1;
+    _Bool reverse = false;
+
+    if (argc < 2 || argc > 3) {
+    	printf("\x1b[1;31mError!\x1b[0m Incorrect number of arguments.\n");
+	    printf("Usage: stretch LENGTH [--decode]\n");
+    	return 1;
+    }
+
+    char * endpt;
+    LEN = strtol(argv[1], &endpt, 10);
+
+    if (LEN < 1 || endpt == argv[1]) {
+    	printf("\x1b[1;31mError!\x1b[0m Incorrect value of length: %s\n", argv[1]);
+	    printf("Length should be greater than 1.\n");
+        return 1;
+    }
+
     
+
     return 0;
 }
